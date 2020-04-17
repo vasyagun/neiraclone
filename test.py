226352af87381351790ef01e54f9e5d73a97f5ef
vasyagun <63818342+vasyagun@users.noreply.github.com>
2020-04-17 12:52:20 +0300

    Update test.py

--- test.py
+++ test.py
@@ -18,7 +18,7 @@ async def on_message(message):
         emb = discord.Embed(title = 'Информация о катализаторе для оружия Шип', colour = discord.Color.dark_gold())
         emb.set_thumbnail(url= 'https://bungie.net/common/destiny2_content/icons/f1144dc4e0d95d87d63ecedc4d31c84c.jpg')
         emb.add_field(name = 'Title', value= 'Скорбь... Готовли ты познать всю глубину отчаяния?' )
-        emb.add_field(name= 'Условие для получения', value= 'Группой из 3 стражей отправится в Яму ереси, вооружившись оружеем скорби. Убить Зулмака Оридее Пыток ни разу не умерев.')
+        emb.add_field(name= 'Условие для получения', value= 'Группой из 3 стражей, вооружившись оружеем скорби. Убить Зулмака Орудее Пыток ни разу не умерев')
         emb.add_field(name= 'Задание катализатора', value= 'Убить стража в патрульной зоне, в любой точке Солнечной системы')
         emb.add_field(name= 'Бонус катализатора', value= 'Повышает дальность, стабильность. А также скрытый перк Взглять тьмы - позволяет видеть носителей света в радиусе 20 метров')
         await channel.send(embed = emb)
