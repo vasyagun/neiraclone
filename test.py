c394c4ff34cf6fe54e1dd542da8e25bc3c280c73
vasyagun <63818342+vasyagun@users.noreply.github.com>
2020-06-19 11:22:07 +0300

    Update test.py

--- test.py
+++ test.py
@@ -126,7 +126,27 @@ async def on_message(message):
 
         await channel.send(embed = emb)
 
-       
+ if message.content.startswith('!правила '):
+
+        await channel.send(f'Eyes up!,{author.name} Рады видеть тебя сдесь!')
+        
+        emb = discord.Embed(title = 'Небольшой вводный инструктаж', colour = discord.Color.dark_gold())
+        emb.set_thumbnail(url= 'https://d.newsweek.com/en/full/1582182/us-health-virus-briefing.jpg?w=790&f=a9624484819a8f1a99321f2425232bb0')
+        emb.add_field(name= 'q: Что это за место?', value= 'a: Сервер для выдоцев ИС и их друзей.',  inline= False)
+        emb.add_field(name= 'q: Зачем это надо?', value= 'a: Попытка избежать: репресий, застоя, безразличия, токсичности.', inline= False)
+        emb.add_field(name= 'q: А надо вступать в клан? ', value= 'a: Необязательно. Можешь состоять в чем хочешь: клан, пати, Единая Россия. Но если чешется, вот ссылка https://www.bungie.net/ru/ClanV2?groupid=4271047 ', inline= False)
+        emb.add_field(name= 'q: А что с ролями?', value= 'Любую роль вы можете получить при обращении в небесную канцелярию(синего цвета)')
+        emb.add_field(name= '@Админ', value= 'кто это вообще?')
+        emb.add_field(name= '@Модер', value= 'а это зачем?')
+        emb.add_field(name= '@вопрос', value= 'если что-то интересует, можно упомянуть в любом канале и получить ответ.')
+        emb.add_field(name= '@Техно Ведьма', value= 'верные подруги Мары Сов :9509_Mara_Sov_Joy:')
+        emb.add_field(name= '@Страж', value= 'стандартная роль')
+        emb.add_field(name= '@pvp God', value= 'роль для сасирисных бойцов с КДА>0.95')
+        emb.add_field(name= '@Helper', value= 'человечек который может протащить по большинству квестов/активностей')
+        emb.add_field(name= '@Криптах', value= 'шарит за все, что происходит в игре. Адепт видосов MadnessBuccanner')
+      
+        
+        await channel.send(embed = emb)       
 
    
 token = os.environ.get('BOT_TOKEN')
